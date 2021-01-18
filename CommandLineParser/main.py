f9847c6f752123276f007027900d9ffa9b2dfa94
Andrey Takhtamirov <andrey1229@icloud.com>
2021-01-17 19:56:44 -0500

    Added time adjustment and time-content grouping

--- CommandLineParser/main.py
+++ CommandLineParser/main.py
@@ -1,12 +1,17 @@
 import json
-
+import datetime
 
 def main():
     with open("message_1.json") as json_file:
         data = json.load(json_file)
-        print(data)
+        # print(data)
         for p in data['participants']:
             print('participants: ' + p['name'])
+
+        for p in data['messages']:
+            if 'content' in p and p['content'] != "":
+                print('time: ' + datetime.datetime.fromtimestamp(p['timestamp_ms'] / 1000).strftime('%Y-%m-%d %H:%M'))
+                print('content: ' + p['content'])
 
 
 # Press the green button in the gutter to run the script.
