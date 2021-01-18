a779df54b084b4556bcdff4913c6edfa1b8df5bf
Andrey Takhtamirov <andrey1229@icloud.com>
2021-01-17 23:22:24 -0500

    Added sender info

--- CommandLineParser/main.py
+++ CommandLineParser/main.py
@@ -1,6 +1,7 @@
 import json
 import datetime
 
+
 def main():
     with open("message_1.json") as json_file:
         data = json.load(json_file)
@@ -8,10 +9,14 @@ def main():
         for p in data['participants']:
             print('participants: ' + p['name'])
 
+        print('\n')
+
         for p in data['messages']:
             if 'content' in p and p['content'] != "":
+                print('sender: ' + p['sender_name'])
                 print('time: ' + datetime.datetime.fromtimestamp(p['timestamp_ms'] / 1000).strftime('%Y-%m-%d %H:%M'))
                 print('content: ' + p['content'])
+                print('\n')
 
 
 # Press the green button in the gutter to run the script.
